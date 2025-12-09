78a032a9e7eaa4daa775847bf38b988baf095f3c
Rannn <astc720@gmail.com>
2025-12-09 09:15:42 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.status(200).json({ msg: "ini halaman utama" });
+});
+
+app.get("/products", (req, res) => {
+  res.status(200).json({
+    msg: "ini halaman product",
+    product1: "product1",
+    product2: "product2",
+  });
+});
+
+app.listen(5000, () => {
+  console.log("server runing");
+});
